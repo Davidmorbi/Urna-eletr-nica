9d9c489a915db3c9ae9104c8b6c8933c742211ab
Davi conceição moreira rosa <122834339+Davidmorbi@users.noreply.github.com>
2023-01-25 19:40:55 -0300

    Urna eletrônica

---
+++ Projeto Urna Eletrônica 1/assets/js/etapas.js
@@ -0,0 +1,65 @@
+let etapas = [
+    {
+        titulo: 'PRESIDENTE',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: "01",
+                nome: 'Luffy',
+                partido: 'Clã mugiwara',
+                fotos: [
+                    {URL: 'assets/images/luffy.png', legenda: 'PRESIDENTE'},
+                    {URL: 'assets/images/brook.png', legenda: 'VICE', small: true}
+                ]
+            },
+            {
+                numero: "02",
+                nome: 'Zoro',
+                partido: 'Clã dos Espadachins',
+                fotos: [
+                    {URL: 'assets/images/zoro.png', legenda: 'PRESIDENTE'},
+                    {URL: 'assets/images/nico robin.png', legenda: 'VICE', small: true}
+                ]
+            },
+            {
+                numero: "03",
+                nome: 'sanji',
+                partido: 'Clã dos Cozinheiros',
+                fotos: [
+                    {URL: 'assets/images/sanji.png', legenda: 'PRESIDENTE'},
+                    {URL: 'assets/images/jinbe.png', legenda: 'VICE', small: true}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'GOVERNADOR',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: "10",
+                nome: 'Ussop',
+                partido: 'Clã dos Mecânicos',
+                fotos: [
+                    {URL: 'assets/images/ussop.png', legenda: 'GOVERNADOR'}
+                ]
+            },
+            {
+                numero: "11",
+                nome: 'Chopper',
+                partido: 'Clã dos médicos',
+                fotos: [
+                    {URL: 'assets/images/chopper.png', legenda: 'GOVERNADOR'}
+                ]
+            },
+            {
+                numero: "12",
+                nome: 'Nami',
+                partido: 'Clã dos Navegadores ',
+                fotos: [
+                    {URL: 'assets/images/nami.png', legenda: 'GOVERNADOR'}
+                ]
+            },
+        ]
+    },
+]
